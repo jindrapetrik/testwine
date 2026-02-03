a0bc04e11b53ce35e82241e224d36d9a2204c0db
Jindra Petřík <jindra.petrik@gmail.com>
2026-02-04 00:39:29 +0100

    try

--- cicd_scripts/script.sh
+++ cicd_scripts/script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+ls -la
+ls -la $CICD_TEMP
 mkdir -p $CICD_TEMP/tools/wine
 cp $CICD_TEMP/hello.exe $CICD_TEMP/tools/wine/hello.exe
 wine $CICD_TEMP/tools/wine/hello.exe
